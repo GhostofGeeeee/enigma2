01be574db52db08c82507f58b23100bcca604ad6
GhostofGeeeee <130693485+GhostofGeeeee@users.noreply.github.com>
2026-05-14 17:36:10 +0000

    code format

--- lib/python/Components/Renderer/VtiImageVersion.py
+++ lib/python/Components/Renderer/VtiImageVersion.py
@@ -6,20 +6,20 @@ from Components.VariableText import VariableText
 
 class VtiImageVersion(VariableText, Renderer):
 
-    GUI_WIDGET = eLabel
+	GUI_WIDGET = eLabel
 
-    def __init__(self):
-        Renderer.__init__(self)
-        VariableText.__init__(self)
+	def __init__(self):
+		Renderer.__init__(self)
+		VariableText.__init__(self)
 
-    def connect(self, source):
-        Renderer.connect(self, source)
-        self.changed((self.CHANGED_DEFAULT,))
+	def connect(self, source):
+		Renderer.connect(self, source)
+		self.changed((self.CHANGED_DEFAULT,))
 
-    def changed(self, what):
-        if what[0] != self.CHANGED_CLEAR:
-            self.text = self.ATVImageVersion()
+	def changed(self, what):
+		if what[0] != self.CHANGED_CLEAR:
+			self.text = self.ATVImageVersion()
 
-    def ATVImageVersion(self):
-        atvversion = BoxInfo.getItem("imageversion")
-        return f"OpenATV Image Release v. {atvversion}"
+	def ATVImageVersion(self):
+		atvversion = BoxInfo.getItem("imageversion")
+		return f"OpenATV Image Release v. {atvversion}"
